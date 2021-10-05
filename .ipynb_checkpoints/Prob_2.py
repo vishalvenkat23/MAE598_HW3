19ceabce24e33194948c51b22cc1ff6311d48a80
vishalvenkat23 <vvenka52@asu.edu>
2021-10-04 21:39:21 -0700

    Problem 2 finally some output 1.1

--- .ipynb_checkpoints/Prob_2.py
+++ .ipynb_checkpoints/Prob_2.py
@@ -5,7 +5,3 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import Matern
 from modAL.models import BayesianOptimizer
 from modAL.acquisition import optimizer_EI, max_EI
-def black_func(x, y):
-    return 4 - 2.1 * x ** 2 + ((x ** 4) / 3) * x ** 2 + x * y + (-4 + 4 * y ** 2) * y ** 2
-
-
